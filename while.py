9e16b78deba4d5e8439d31b6f3679b5c08660d8b
analy0120 <analysantos220410@gmail.com>
2025-08-04 11:48:32 -0300

    exercicios de while

---
+++ while.py
@@ -0,0 +1,54 @@
+x=1
+while x <= 50000:
+    print(x)
+    print('continua...')
+    x = x + 1
+  
+x=1
+while x <= 100:
+    print(x)
+    print('continua...')
+    x = x + 1
+     
+x=50 
+
+while x <= 100:
+    print(x)
+    print('continua...')
+    x = x + 50
+
+def  exercicios_3():
+    x = 10 
+    while x >= 0:
+        print(x)
+        x = x - 1 
+    print('fogo!')
+exercicios_3() 
+
+
+
+fim=int(input("digite o ultimo numero a imprimir:"))
+x = 0 
+while x <= fim:
+    if x % 2 == 0:
+        print(x)
+    x = x + 1 
+
+
+    fim = 9
+    x = 0 
+    while x <= fim:
+        if x % 2 == 0:
+            print()
+    else:
+        print(x)
+    x = x +1 
+ 
+def arvore_de_natal():
+    linha = 1
+    while linha <= linha:
+        print('💙', end="")
+        coluna = coluna + 1
+    print()
+    linha = linha + 1 
+arvore_de_natal()
